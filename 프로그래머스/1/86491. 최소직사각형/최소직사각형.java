736aa186542ed6e17eccf7fd3b117a48419bbb24
i-am-jiwon <141749436+i-am-jiwon@users.noreply.github.com>
2024-04-11 08:57:37 +0900

    Update 최소직사각형.java
    min max 추가

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -24,6 +24,24 @@ class Solution {
             if(sizes[i][1] > right) right = sizes[i][1];
         }     
         
-       return left*right;
+        // for문으로 제출하기
+        // return left*right;
+        
+        
+        // min max 활용한 풀이
+        
+        int maxWidth = 0;
+        int maxHeight = 0;
+    
+        // 주어진 명함의 가로와 세로 길이를 확인하여 가장 긴 길이를 찾음
+        for (int[] size : sizes) {
+            // 주어진 두 값 중 큰 값과 maxWidth 비교 후 큰 값을 가로로 저장
+            maxWidth = Math.max(maxWidth, Math.max(size[0], size[1]));
+            // 주어진 두 값 중 작은 값과 maxWidth 비교 후 큰 값을 세로로 저장
+            maxHeight = Math.max(maxHeight, Math.min(size[0], size[1]));
+        }
+    
+        // 가장 작은 지갑의 크기는 가장 긴 가로 길이와 세로 길이를 곱한 것이 됨
+        return maxWidth * maxHeight;
     }
 }
