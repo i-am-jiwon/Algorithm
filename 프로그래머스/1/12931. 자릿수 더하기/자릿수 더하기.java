f1f4badd96f2f027b8afe5e240ea95a2942b5133
i-am-jiwon <141749436+i-am-jiwon@users.noreply.github.com>
2024-04-12 18:39:29 +0900

    [level 1] Title: 자릿수 더하기, Time: 2.52 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -5,8 +5,14 @@ import java.util.stream.IntStream;
 
 public class Solution {
     public int solution(int n) {
-        return IntStream.iterate(n, i -> i / 10)
-            .takeWhile(i -> i > 0)
+        /*
+            IntStream을 활용한 풀이
+            각 자리 수의 합을 위해 10으로 나눠서 10 보다 크면 총 합에 추가하기
+        */
+        
+        //iterate로 n을 10으로 나누기
+        return IntStream.iterate(n, i -> i > 0, i -> i / 10)
+            // 
             .map(i -> i % 10)
             .sum();
     }
