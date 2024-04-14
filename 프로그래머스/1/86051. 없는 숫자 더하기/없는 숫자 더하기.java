8c0c12cf7868874fe2dad785c7654f27b01b8959
i-am-jiwon <141749436+i-am-jiwon@users.noreply.github.com>
2024-04-14 14:51:51 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 2.95 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -3,9 +3,20 @@ import java.util.stream.*;
 class Solution {
     public int solution(int[] numbers) {
         
+        /*
+            intStream을 이용하여 numbers 배열을 받고
+            총합을 0~9까지의 합인 45에서 뺐다.
+        */
         
+        return 45 - IntStream.of(numbers).sum();
         
-        int answer = 45 - IntStream.of(numbers).sum(); 
-        return answer;
+        /*
+            다른 풀이
+            Arrays.stream() - 배열을 기본 스트림으로 변환 / 좀 더 범용적이다. < 다양한 배열에 사용할 수 있다.
+            IntStream.of() - 주어진 값들을 스트림으로 변환
+        */
+        
+        // return 45-Arrays.stream(numbers).sum();
+        
     }
 }
