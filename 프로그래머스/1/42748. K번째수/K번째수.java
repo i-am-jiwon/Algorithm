e37db08929b04005266541ef36749a927c24fe57
i-am-jiwon <141749436+i-am-jiwon@users.noreply.github.com>
2024-04-15 17:29:31 +0900

    [level 1] Title: K번째수, Time: 7.09 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,23 +3,13 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
-        answer = IntStream.range(0,commands.length)
+        
+        return IntStream.range(0,commands.length)
             .map (i-> Arrays.stream(array)
                 .skip(commands[i][0]-1)
                 .limit(commands[i][1]-commands[i][0]+1)
                 .sorted()
                 .toArray()[commands[i][2]-1])
             .toArray();
-
-       /* IntStream.range(0,commands[commands.length-1][2])
-            .map (i-> Arrays.stream(array)
-                .skip(commands[i][0]-1)
-                .limit(commands[i][1]-commands[i][0]+1)
-                .sorted()
-                .toArray()[commands[i][2]])
-            .toArray();
-        */
-        return answer;
     }
 }
