7faed1934660bae90af9486c4e56e19a382b9b16
i-am-jiwon <141749436+i-am-jiwon@users.noreply.github.com>
2024-04-16 13:52:16 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 9.67 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -8,5 +8,13 @@ class Solution {
         return s.length() %2 == 1 
             ? "" +s.charAt(s.length()/2) 
             : "" + s.charAt(s.length()/2 -1) +s.charAt(s.length()/2);
+        
+        /*
+           substring을 활용하여  
+           (s.length()-1)/2 초기조건에서 홀수와 짝수 모두 작동하게 만든 코드
+        */
+        // return s.substring((s.length()-1)/2, s.length()/2 +1);
+        
+        
     }
 }
