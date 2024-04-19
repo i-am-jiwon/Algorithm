afa9c92a284b2de58293378a471b49df7d59320a
임지원 <141749436+i-am-jiwon@users.noreply.github.com>
2024-04-19 15:37:52 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 6.08 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,14 +1,24 @@
+import java.util.Arrays;
+
 class Solution {
     public String solution(String[] seoul) {
-        String answer = "";
         
-        System.out.println(seoul.length);
-        
+        /*
+            for문을 활용하여 seoul배열에서 Kim과 일치하는 인덱스를 찾아냄
+        */
         
         for(int i = 0; i < seoul.length; i++){
             if(seoul[i].equals("Kim")) return "김서방은 "+ i+"에 있다" ;
         }
         
         return "김서방은 "+ seoul.length+"에 있다." ;
+        
+        
+        /*
+            Arrays의 asList를 활용하여 seoul의 index를 가져오기
+        */
+        
+        // return "김서방은 "+ Arrays.asList(seoul).indexOf("Kim")+"에 있다." ;
+        
     }
 }
