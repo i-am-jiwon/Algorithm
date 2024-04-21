f586f7bb0f30adf7f3461daa44213f9679451638
임지원 <141749436+i-am-jiwon@users.noreply.github.com>
2024-04-21 20:47:50 +0900

    [level 0] Title: 옹알이 (1), Time: 0.16 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,9 +1,15 @@
 class Solution {
     public int solution(String[] babbling) {
-        int answer = 0;
         
-        System.out.println(babbling[0]);
-        System.out.println(babbling.length);
+        /*
+            배열의 문자열에서 조건에 해당하는 문자열을 빼준다.
+            공백일 경우 조건에 부합하는 문자열로만 이루어졌다고 판단한다.
+            
+            wyeoo의 경우 ye를 빼주면 woo로 바뀌기 때문에 조건에 부합한다고 잘못 판단된다.
+            따라서 조건의 문자열을 빼는것이 아니라 " " 공백으로 바꾸고 마지막에 trim()으로 공백만 있는 경우를 제거한다.
+        */
+        
+        int answer = 0;
         
         for(int i = 0; i < babbling.length; i++){
             babbling[i]= babbling[i].replace("aya", " ");
@@ -12,8 +18,6 @@ class Solution {
             babbling[i]= babbling[i].replace("ma", " ");
             
             if(babbling[i].trim().length() == 0) answer++;
-            System.out.println(babbling[i].trim().length());
-
         }
         
         return answer;
